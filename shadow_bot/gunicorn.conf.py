0d1941a5f6821e0d6da0320abefadcb0f5d057ef
Charles Fauchet <charlesfauchet@Charless-MacBook-Pro.local>
2025-03-07 15:25:16 +0800

    Removed botv6 from gunicorn command

--- shadow_bot/gunicorn.conf.py
+++ shadow_bot/gunicorn.conf.py
@@ -10,11 +10,11 @@ import os
 # loglevel = 'debug'
 
 # Custom setup to run botv6.py
-def on_starting(server):
-    """Run the bot script when Gunicorn starts."""
-    # Set up Django environment for the bot script
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shadow_bot.settings.dev")
+# def on_starting(server):
+#     """Run the bot script when Gunicorn starts."""
+#     # Set up Django environment for the bot script
+#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shadow_bot.settings.dev")
 
-    # Start the bot script as a subprocess
-    bot_process = subprocess.Popen(["python", "botv6.py"])
-    server.log.info(f"Started botv6.py with PID {bot_process.pid}")
+#     # Start the bot script as a subprocess
+#     bot_process = subprocess.Popen(["python", "botv6.py"])
+#     server.log.info(f"Started botv6.py with PID {bot_process.pid}")

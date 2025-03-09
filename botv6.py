09ec47d9f384bbc9e1036422f5b62ef38d850ba5
Charles Fauchet <charlesfauchet@Charless-MacBook-Pro.local>
2025-03-09 09:52:37 +0800

    Added start command

--- botv6.py
+++ botv6.py
@@ -295,7 +295,21 @@ async def alert_creator(user_chat_id, client_username):
 #         f"Human takeover OFF for client `{client_username}` under creator `{creator_username}`."
 #     )
 
-
+def start(update: Update, context: CallbackContext) -> None:
+	user_id = update.effective_user.id 
+	try:
+		user_data = UserData.objects.get(id=user_id)
+		update.message.reply_text(f'Welcome back {user_data.first_name}!')
+	except UserData.DoesNotExist:
+		new_user = UserData(
+			id = user_id,
+			first_name = update.effective_user.first_name,
+			last_name = update.effective_user.last_name,
+			username = update.effective_user.username
+		)
+		new_user.save()
+		update.message.reply_text(f'Welcome to ShadowMe!')
+        
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # print('inspect chat triggered')
     # chat_id = update.effective_chat.id
